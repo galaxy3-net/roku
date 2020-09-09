f72bfcd0797da8007e0750a1946382b9d98478fc
Korben Kirscht <kirscht@gmail.com>
2020-09-08 21:30:13 -0700

    Update feed

--- create.py
+++ create.py
@@ -16,6 +16,37 @@ my_feed['categories'] = [ {
     "order": "most_popular"
 } ]
 
+my_feed['movies'] = [
+{  
+   "id":"1509428502952",
+   "title":"Sample Movie",
+   "content":{  
+  	"dateAdded":  datetime.now().astimezone().replace(microsecond=0).isoformat(),
+  	"videos": [
+   	 {
+       		"url": "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8",
+       	  	"quality": "HD",
+       	 	"videoType": "MP4"
+     	 }
+	]
+   },
+   "genres":[  
+      "drama",
+      "comedy",
+      "horror"
+   ],
+   "thumbnail":"https://example.org/cdn/thumbnails/1509428502952/1",
+   "releaseDate":"2016-01-01",
+   "shortDescription":"Incredible movie description",
+   "longDescription":"Even more incredible and longer movie description",
+   "tags":[  
+      "amazing",
+      "drama",
+      "comedy",
+      "horror"
+   ]
+}]
+
 json_obj = json.dumps(my_feed, indent=4)
 
 fh = open("feed.json", "w")
